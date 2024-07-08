e06ef7ced62f579618a41795f3879b90ad7bdec0
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-07-08 17:25:44 +0300

    first commit

---
+++ src/main/java/com/example/sber_ai/SberAiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sber_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SberAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SberAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sber_ai/SberAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sber_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SberAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

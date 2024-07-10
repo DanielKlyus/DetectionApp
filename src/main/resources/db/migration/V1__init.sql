3620f2cb0dfa9f5c7541553cc78caceb79fe3d02
DanielKlyus <d.klyus@g.nsu.ru>
2024-07-10 10:55:45 +0700

    change sql

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -1,13 +1,15 @@
 CREATE TABLE users (
-	"id"		serial		PRIMARY KEY	NOT NULL,
-	"name"		TEXT		NOT NULL,
-	"is_admin"  BOOLEAN		NOT NULL
+	"id"		    serial		PRIMARY KEY	NOT NULL,
+	"name"		    TEXT		NOT NULL,
+	"is_admin"      BOOLEAN		NOT NULL
 );
 
 CREATE TABLE projects (
-	"id" 		serial		PRIMARY KEY NOT NULL,
-	"name"		TEXT		NOT NULL,
-	"user_id"	INTEGER		NOT NULL,
+	"id" 		    serial		PRIMARY KEY NOT NULL,
+	"name"		    TEXT		NOT NULL,
+	"path_source"   TEXT        NOT NULL,
+	"path_save"     TEXT        NOT NULL,
+	"user_id"	    INTEGER		NOT NULL,
 
 	CONSTRAINT fk_projects_user FOREIGN KEY ("user_id") REFERENCES users ("id")
 );

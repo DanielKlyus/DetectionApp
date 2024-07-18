827c2850edaaea89333d9262e0c0634e40b73ecf
DanielKlyus <d.klyus@g.nsu.ru>
2024-07-18 12:54:45 +0700

    add request mapping

--- src/main/java/com/example/sber_ai/controller/ProjectController.java
+++ src/main/java/com/example/sber_ai/controller/ProjectController.java
@@ -6,14 +6,16 @@ import com.example.sber_ai.service.ProjectService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/projects")
 public class ProjectController {
     private final ProjectService projectService;
 
-    @PostMapping("projects/create")
+    @PostMapping("/create")
     public ProjectResponse createProject(@RequestBody ProjectRequest projectRequest) {
         return projectService.createProject(projectRequest);
     }
--- src/main/java/com/example/sber_ai/controller/UserController.java
+++ src/main/java/com/example/sber_ai/controller/UserController.java
@@ -6,14 +6,16 @@ import com.example.sber_ai.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
     private final UserService userService;
 
-    @PostMapping("users/create")
+    @PostMapping("/create")
     public UserResponse createUser(@RequestBody UserRequest userRequest) {
         return userService.createUser(userRequest);
     }

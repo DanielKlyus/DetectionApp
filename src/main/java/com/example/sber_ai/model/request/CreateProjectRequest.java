9f39aae44d595c37a18b9f59632c87f2dd7895fc
Авдеев Виталий Сергеевич <v.avdeev@g.nsu.ru>
2024-08-01 11:56:06 +0300

    Validation

--- src/main/java/com/example/sber_ai/model/request/CreateProjectRequest.java
+++ src/main/java/com/example/sber_ai/model/request/CreateProjectRequest.java
@@ -1,5 +1,7 @@
 package com.example.sber_ai.model.request;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,11 +12,15 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateProjectRequest {
+    @NotBlank(message = "Name cannot be empty")
     private String name;
 
+    @NotBlank(message = "Path source cannot be empty")
     private String pathSource;
 
+    @NotBlank(message = "Path save cannot be empty")
     private String pathSave;
 
+    @NotNull(message = "User id cannot be null")
     private Long userId;
 }
